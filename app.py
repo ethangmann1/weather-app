c198105f84f3ebb421e9c2d1b3b3d720b5a59444
ethangmann1 <119371277+ethangmann1@users.noreply.github.com>
2023-01-11 02:40:24 -0500

    Working Version!!
    It works, changed the units from kelvin to imperial, deleted the divide by ten part as I was getting the units wrong, the code is fully working now.

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ api_key = '99999f5fe42029bc4bea2be46b101327'
 user_input = input("Enter city: ")
 
 weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}")
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&appid={api_key}")
 if weather_data.json() ['cod'] == '404':
     print("No city found")
 else:
     weather = weather_data.json()['weather'][0]['main']
-    temp = round(weather_data.json()['main']['temp']) // 10
+    temp = round(weather_data.json()['main']['temp'])
 
     print (f"The weather in {user_input} is looking like: {weather}")
     print (f"The temperature in {user_input} is looking like: {temp}°F")
