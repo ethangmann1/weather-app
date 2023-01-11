46d6ed5844357a407e475c2194bef7eeafa4d123
ethangmann1 <119371277+ethangmann1@users.noreply.github.com>
2023-01-11 02:13:43 -0500

    working API call back

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ user_input = input("Enter city: ")
 
 weather_data = requests.get(
     f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}")
+
 #(this is the API call from OpenWeather)
-print (weather_data.status_code)
