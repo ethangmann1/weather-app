ed7cef0955876ea58b257918984a824c44b7c1bd
ethangmann1 <119371277+ethangmann1@users.noreply.github.com>
2023-01-11 02:29:41 -0500

    Working weather data
    Added a division command to temp to divide it by ten. Before it would be 270 degrees for example, now it is 27.

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ user_input = input("Enter city: ")
 
 weather_data = requests.get(
     f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}")
+if weather_data.json() ['cod'] == '404':
+    print("No city found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp']) // 10
 
-#(this is the API call from OpenWeather)
-
-weather = weather_data.json()['weather'][0]['main']
-temp = round(weather_data.json()['main']['temp'])
-
-print (f"The weather in {user_input} is looking like: {weather}")
-print (f"The temperature in {user_input} is looking like: {temp}°F")
+    print (f"The weather in {user_input} is looking like: {weather}")
+    print (f"The temperature in {user_input} is looking like: {temp}°F")
