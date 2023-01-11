db650d6ef3c8d9fc05368fa836d592477ebdea5e
ethangmann1 <119371277+ethangmann1@users.noreply.github.com>
2023-01-11 02:22:59 -0500

    User input print commands
    added lines to print a sentence with the temperature and the weather

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ weather_data = requests.get(
     f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}")
 
 #(this is the API call from OpenWeather)
+
+weather = weather_data.json()['weather'][0]['main']
+temp = round(weather_data.json()['main']['temp'])
+
+print (f"The weather in {user_input} is looking like: {weather}")
+print (f"The temperature in {user_input} is looking like: {temp}°F")
