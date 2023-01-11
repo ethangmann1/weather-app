e83a5d300578379ff0f297c691507d8598aec4bb
ethangmann1 <119371277+ethangmann1@users.noreply.github.com>
2023-01-11 02:09:34 -0500

    API Call added
    added API call into code

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import requests
 api_key = '99999f5fe42029bc4bea2be46b101327'
 
 user_input = input("Enter city: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}")
+#(this is the API call from OpenWeather)
+print (weather_data.status_code)
